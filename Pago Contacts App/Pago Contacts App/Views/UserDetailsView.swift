a1b3d65dcc27194ce6d2b91b03da4b6581e65497
Santamarian <santamarian.bogdan@gmail.com>
2024-03-18 13:54:54 +0200

    add functionality for splitting user.name in name, surname and display them in the proper textfields

--- Pago Contacts App/Pago Contacts App/Views/UserDetailsView.swift
+++ Pago Contacts App/Pago Contacts App/Views/UserDetailsView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 
 struct UserDetailsView: View {
     
+    // MARK: - Properties
     @Bindable var user: User
+    
+    @State private var name: String = ""
+    @State private var surname: String = ""
     
     var body: some View {
         Form {
-            TextField("name".localized, text: $user.name)
-            TextField("surname".localized, text: $user.surname.toUnwrapped(defaultValue: ""))
+            TextField("name".localized, text: $name)
+            TextField("surname".localized, text: $surname)
             TextField("phone".localized, text: $user.phoneNumber.toUnwrapped(defaultValue: ""))
             TextField("email".localized, text: $user.email)
         }
+        .onAppear {
+            if let (surname, name) = splitName(user.name) {
+                self.surname = surname
+                self.name = name
+            }
+        }
+        .onDisappear {
+            updateUserFullName()
+        }
         .navigationTitle($user.name)
+    }
+    
+    // MARK: - Private methods
+    private func splitName(_ fullName: String) -> (String, String)? {
+        let components = fullName.components(separatedBy: " ")
+        guard components.count == 2 else {
+            return nil
+        }
+        return (components[0], components[1])
+    }
+    
+    private func updateUserFullName() {
+        user.name = "\(surname) \(name)"
     }
 }
 
